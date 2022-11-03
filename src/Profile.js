8032402b41efa82ce0513bd0aff2460bab726e59
ifeanyihm <iheme.ifeanyi@yahoo.com>
2022-11-03 18:33:24 +0100

    modified icon

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,12 @@
-import avatar_web from "./Icons/Avatar_share.svg"
-import avatar_mobile from "./Icons/Avatar_shareb.svg"
+import web from "./Icons/Avatar_share.svg"
+import mobile from "./Icons/Avatar_shareb.svg"
 
 const Profile = () => {
     return ( 
         <section className="profile">
             <div id="profile__img" tabindex="1" style={{width:'88px', height:'88px'}}></div>
-            <img className="avatar_mobile" src={avatar_mobile} alt="nav icon" />
-            <img className="avatar_web" src={avatar_web} alt="nav icon" />
+            <img className="avatar_mobile" src={mobile} alt="nav icon" />
+            <img className="avatar_web" src={web} alt="nav icon" />
             <p id="twitter">Ifeanyi Michael</p>
             <p id="slack" style={{display:'none'}}>Ifeanyi Iheme</p>
         </section>
